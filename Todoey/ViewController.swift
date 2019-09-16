41c563139002d8548ccd9fbaf1858125135c8405
Taichi Ogi <timeismoney529@i.softbank.jp>
2019-09-16 15:48:17 +0900

    Initial Commit

---
+++ Todoey/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Todoey
+//
+//  Created by ogi on 2019/09/16.
+//  Copyright © 2019 Taichi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
